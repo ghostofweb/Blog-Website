f58de57e65cf035c4485723e54d6c6250a643d37
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-10-21 18:04:08 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true, // Enable fallback for HTML5 history API
+  },
+});
